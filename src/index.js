d4c41f6f2716bfe15715a0e9076e3e921cd8ed26
thistonyb <this.tonybaker@gmail.com>
2019-10-01 13:01:44 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('%c HI', 'color: firebrick')
+console.log("%c HI", "color: firebrick");
+document.addEventListener("DOMContentLoaded", function() {
+  fetchBreeds();
+});
+
+function fetchBreeds() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+  fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => renderBreeds(json));
+  return fetch(imgUrl);
+}
+
+function renderBreeds(json) {
+  const imgContain = document.getElementById("dog-image-container");
+  json.forEach(breed => {
+    const image = document.createElement("img");
+    image.src = `${breed}`;
+    imgContain.appendChild(image);
+  });
+}
